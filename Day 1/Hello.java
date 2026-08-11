dc6b06ef2e4fb69cf8fa1c6df26d3386ad7c1fb3
Ishika Thakur <ishikathakur0708@gmail.com>
2026-08-11 20:31:35 +0530

    basic hello.java

---
+++ Day 1/Hello.java
@@ -0,0 +1,5 @@
+public class Hello {   //the class name should be identical to the file name you're saving as .java
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
